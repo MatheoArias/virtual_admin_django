bbaab3e9c61cd52aed5fce1a4ed70551c98877a9
MatheoArias <teoarco@gmail.com>
2025-05-07 09:47:06 -0500

    I add media files to local.py, production.py and base.py

--- apps/liable/models.py
+++ apps/liable/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Position(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    code=models.CharField('Código', max_length=10, blank=True, null=False)
+    name=models.CharField('Nombre', max_length=100,blank=True, null=False)
+    lastName=models.CharField("Apellidos", max_length=100, blank=True, null=False)
+    email=models.EmailField('Correo Electrónico', max_length=254, blank=True, null=False)
+    telephone=models.CharField('Teléfono', max_length=10, blank=False, null=False )
+    workstation=models.CharField("Ubicación", max_length=50, blank=True, null=True)
+    
+       
--- virtual_admin/settings/local.py
+++ virtual_admin/settings/local.py
@@ -44,3 +44,6 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
